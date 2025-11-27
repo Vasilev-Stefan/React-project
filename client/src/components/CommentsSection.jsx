463d3138b8cfb4411b0678c2700ce920d3e6156b
Vasilev-Stefan <sbasilev@gmail.com>
2025-11-28 00:37:33 +0200

    Display dynamic comments on each game

--- client/src/components/CommentsSection.jsx
+++ client/src/components/CommentsSection.jsx
@@ -1,17 +1,30 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router"
+
 export function CommentsSection() {
+    const { id } = useParams()
+
+    const [comments, setComments] = useState([])
+
+    useEffect(() => {
+        let allComments = {}
+        fetch(`http://localhost:3030/jsonstore/comments`)
+        .then(response => response.json())
+        .then(result => setComments(Object.values(result).filter(c => c.gameId === id)))
+        .catch(error => alert(error.message))
+
+    }, [setComments])
+
     return (
         <div className="details-comments">
             <h2>Comments:</h2>
+            {comments.length > 0 ?
             <ul>
-                <li className="comment">
-                    <p>Content: A masterpiece of world design, though the boss fights are brutal.</p>
-                </li>
-                <li className="comment">
-                    <p>Content: Truly feels like a next-gen evolution of the Souls formula!</p>
-                </li>
-            </ul>
-            {/* <!-- Display paragraph: If there are no games in the database --> */}
-            {/* <!-- <p className="no-comment">No comments.</p> --> */}
+                {comments.map(c => <li className="comment" key={c._id}><p>Content: {c.content}</p></li>)}
+            </ul> 
+            :
+            <p className="no-comment">No comments.</p>
+            }
         </div>
     )
 }
