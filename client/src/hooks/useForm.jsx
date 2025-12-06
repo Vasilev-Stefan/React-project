74e2e0d7bebc623e3ec28f35b7a2fdcc7d4b5dfa
Vasilev-Stefan <sbasilev@gmail.com>
2025-12-07 00:56:34 +0200

    Create useForm hook

---
+++ client/src/hooks/useForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export function useForm(initialValues, onSumbitCallback, validate) {
+  const [data, setData] = useState(initialValues);
+  const [errors, setErrors] = useState({});
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+
+    setData((state) => ({
+        ...state,
+        [name]: value
+    }))
+  };
+
+  const onSubmitHandler = (e) => {
+
+    const validationErrors = validate ? validate(data) : {}
+
+    if(Object.keys(validationErrors).length > 0){
+        setErrors(validationErrors)
+        return;
+    }
+
+    setErrors({})
+    onSumbitCallback(data)
+  }
+
+  return {
+    data,
+    errors,
+    onChangeHandler,
+    onSubmitHandler,
+    setErrors
+  }
+}
+
+
