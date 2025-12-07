da20144aa3db99e02b3602077a59dda0a7a5a978
Vasilev-Stefan <sbasilev@gmail.com>
2025-12-07 12:59:18 +0200

    Add login ability

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react"
 import { useNavigate } from "react-router"
+import { useUser } from "../hooks/useUser"
+import { useFetch } from "../hooks/useFetch"
+import { useForm } from "../hooks/useForm"
 
 const initialData = {
     email: '',
@@ -7,57 +9,51 @@ const initialData = {
 }
 
 export function Login() {
-    const [data, setData] = useState(initialData)
-    const [errors, setErrors] = useState({})
-
+    const { login } = useUser()
+    const { request } = useFetch()
     const navigate = useNavigate()
 
-    const onChangeHandler = (e) => {
-        const name = e.target.name
-        const value = e.target.value
 
-        setData(state => ({
-            ...state,
-            [name]: value,
-        }))
+
+    const validate = (data) => {
+        const erros = {}
+
+        if(!data.email){
+            erros.email = 'Email is required!'
+        }
+
+        if(!data.password){
+            erros.password = 'Password is required!'
+        }
+
+        return erros
     }
 
-    const onSubmitHandler = (e) => {
-        e.preventDefault()
-
-        const newErrors = {}
-
-        if(data.email.length == 0){
-            newErrors.email = 'Email is required!'
+    const loginUser = async (data) => {
+        try {
+            const result = await request('users/login', 'POST', data)
+            login(result)
+            navigate('/')
+        } catch (error) {
+            alert(error.message)
         }
-
-        if(data.password.length == 0){
-            newErrors.password = 'Password is required!'
-        }
-
-        if(Object.keys(newErrors).length > 0){
-            setErrors(newErrors)
-            return
-        }
-
-        setErrors({})
-        console.log('Successful login')
-        navigate('/')
     }
+
+    const { errors, onSubmitHandler, inputFiller } = useForm(initialData, loginUser, validate)
 
     return (
         <section id="login-page">
 
-            <form id="login" onSubmit={onSubmitHandler}>
+            <form id="login" action={onSubmitHandler}>
                 <div className="container">
                     <h1>Login</h1>
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email" onChange={onChangeHandler} value={data.email} name="email" placeholder="Your Email" />
+                    <input type="email" id="email" {...inputFiller('email')} placeholder="Your Email" />
                     {errors.email && <p>{errors.email}</p>}
 
 
                     <label htmlFor="login-pass">Password</label>
-                    <input type="password" id="login-password" onChange={onChangeHandler} value={data.password} name="password" placeholder="Password" />
+                    <input type="password" id="login-password" {...inputFiller('password')} placeholder="Password" />
                     {errors.password && <p>{errors.password}</p>}
 
                     <input type="submit" className="btn submit" value="Login" />
