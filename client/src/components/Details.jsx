e92501e24510b0e84696cfa980e9fb23f923a8a5
Vasilev-Stefan <sbasilev@gmail.com>
2025-12-07 14:01:06 +0200

    Dynamic buttons based on user state in details page

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -1,62 +1,60 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { useParams } from "react-router"
 import { GameButtons } from "./GameButtons"
 import { CommentsSection } from "./CommentsSection"
 import { AddComment } from "./AddComment"
+import { useFetch } from "../hooks/useFetch"
+import { useUser } from "../hooks/useUser"
 
 export function Details() {
-    const {id} = useParams()
-
-    const [game, setGame] = useState({})
+    const {id: gameId} = useParams()
+    const { data: game } = useFetch(`data/games/${gameId}`)
+    const { user, isAuthenticated } = useUser()
     const [forceRefresh, setForceRefresh] = useState(false)
 
     const refresh = () => {
         setForceRefresh(state => !state)
     }
 
-    useEffect(() => {
-        fetch(`http://localhost:3030/jsonstore/games/${id}`)
-        .then(response => response.json())
-        .then(result => setGame(result))
-        .catch(error => alert(error))
-    }, [id])
+    console.log(game)
+
     return (
         <section id="game-details">
             <h1>Game Details</h1>
             <div className="info-section">
 
                 <div className="header-and-image">
-                    <img className="game-img" src={game.imageUrl} alt={game.title} />
+                    <img className="game-img" src={game?.imageUrl} alt={game?.title} />
 
                     <div className="meta-info">
-                        <h1 className="game-name">{game.title}</h1>
+                        <h1 className="game-name">{game?.title}</h1>
 
                         <p className="data-row">
                             <span className="label">Genre:</span>
-                            <span className="value">{game.genre}</span>
+                            <span className="value">{game?.genre}</span>
                         </p>
 
                         <p className="data-row">
                             <span className="label">Active Players:</span>
-                            <span className="value">{game.players}</span>
+                            <span className="value">{game?.players}</span>
                         </p>
 
                         <p className="data-row">
                             <span className="label">Release Date:</span>
-                            <span className="value">{game.date}</span>
+                            <span className="value">{game?.date}</span>
                         </p>
                     </div>
                     <div className="summary-section">
                         <h2>Summary:</h2>
                         <p className="text-summary">
-                            {game.summary}
+                            {game?.summary}
                         </p>
                     </div>
                 </div>
 
 
                 {/* <!-- Edit/Delete buttons ( Only for creator of this game )  --> */}
-                <GameButtons />
+                {isAuthenticated && user?._id === game?._ownerId && <GameButtons />}
 
                 <CommentsSection refresh={refresh} />
 
